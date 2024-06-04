b7dbc8fcaefbb6c2dc332a506b8accaa4206059e
Sean Alphonse <sean@aleksandr.cc>
2024-06-04 02:39:06 -0500

    initial commit

---
+++ src/test/java/io/gohealth/labs/assignment/AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gohealth.labs.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
